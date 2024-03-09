6e3c143aad0e5c4d74c0d09612ef487a0fde410b
daylonblakely <daylonblakely@gmail.com>
2024-03-09 15:07:41 -0800

    init langchain

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,13 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+import { ChatOpenAI } from '@langchain/openai';
 app.get('/', (req: Request, res: Response) => {
+  const chatModel = new ChatOpenAI({});
+  chatModel
+    .invoke('what is LangSmith?')
+    .then((res) => console.log(res))
+    .catch((error) => console.log(error));
   res.send('Hello, Express with TypeScript!');
 });
 

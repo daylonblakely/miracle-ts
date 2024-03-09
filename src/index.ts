7329e043e82854187b44ccd9ad835c6b7c160c39
daylonblakely <daylonblakely@gmail.com>
2024-03-09 14:34:36 -0800

    docker init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
 import app from './app';
 
-const port = 5000;
+// Load environment variables from .env file
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
